12d9b7be998ccf66b7dee677959b7c27856ed906
Awoyemi Patrick <awoyemipatrick33@yahoo.com>
2023-02-17 01:03:24 +0100

    GPT_Portfolio-website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+// import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This is going to allow us to hook our react application into index.html
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This App file is going to be rendered inside index.html 'root'
+import App from './App'
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<App />, document.getElementById('root'));
